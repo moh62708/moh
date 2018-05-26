476e0b6a2f9fc0c9f00083e2db30428209dadbed
moh62708 <34512678+moh62708@users.noreply.github.com>
2018-05-26 02:47:36 +0200

    Create 2018.js

---
+++ 2018.js
@@ -0,0 +1,8 @@
+$(function (){
+  'use strict';
+  $(window) .scroll(function(){
+    var navbar = $('.navbar');
+    ($(window).scrollTop() >= navbar.height() ? navbar.addClass('scrolled') : navbar.removeClass('scrolled')
+
+  })
+})
